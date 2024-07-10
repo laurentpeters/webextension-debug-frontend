5d9baf7ce1b6a906af52dea3a15b26c72a952f25
lape <lape@hms.se>
2024-07-10 13:57:09 +0200

    replace for localhost/authn to allow local debugging

--- helloworld.js
+++ helloworld.js
@@ -1,7 +1,27 @@
-// add style to body
-document.body.style.border = "5px solid red";
 
-// add a new element to the DOM after body
-var p = document.createElement("p");
-p.textContent = "Hello World! This paragraph was added by a content script.";
-document.body.insertBefore(p, document.body.firstChild);
+
+// Select the form with the ID 'kc-form-login'
+var form = document.getElementById('kc-form-login');
+
+if (form) {
+
+    // add style to body
+    document.body.style.border = "5px solid red";
+
+    // add a new element to the DOM after body
+    var p = document.createElement("p");
+    p.textContent = "submit link modified to be relative to the current page.";
+    document.body.insertBefore(p, document.body.firstChild);
+
+    // Get the action attribute of the form
+    var actionURL = form.getAttribute('action');
+
+    // Create a new URL object using the action attribute value
+    var url = new URL(actionURL);
+
+    // Extract the pathname and search from the URL object to get the relative path with query parameters
+    var relativePath = url.pathname + url.search;
+
+    // Set the form's action attribute to the new relative path
+    form.setAttribute('action', relativePath);
+}
